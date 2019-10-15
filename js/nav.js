96acade2e9130037e0e8654599b41e41c008e83e
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2019-10-15 19:40:01 +0200

    added scroll to top button

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 const navigation = document.querySelector(".navigation__links");
 const navigationButton = document.querySelector("#navigation__button");
 const myNav = document.querySelector(".navigation");
+const scrollButton = document.querySelector('#scrollToTop');
 
 const addBackground = () => {
   if (window.pageYOffset >= 500) {
@@ -11,6 +12,14 @@ const addBackground = () => {
     myNav.classList.remove("navigation-colored");
   }
 };
+
+const addScrollButton = () => {
+  if (window.pageYOffset >= 500) {
+    scrollButton.classList.add("active");
+  } else {
+    scrollButton.classList.remove("active");
+  }
+}
 
 const makeResponsive = () => {
   if (navigation.className === "navigation__links") {
@@ -37,4 +46,26 @@ navigationButton.addEventListener("click", () => {
 window.addEventListener("scroll", () => {
   addBackground();
   closeMenu();
+  addScrollButton();
 });
+
+
+//Get the button
+// const scrollButton = document.querySelector('#scrollToTop');
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
