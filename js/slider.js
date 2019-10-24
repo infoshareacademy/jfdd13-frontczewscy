1e3b64e05f2df2b408a298fe7bb5965c2a6b5bf3
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2019-10-24 21:25:22 +0200

    added timeout to the buttons in slider

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,3 @@
-
-
 const heroSlider = document.querySelector('.hero__slider');
 const heroElements = document.querySelectorAll(".hero__item");
 const leftButton = document.querySelector(".left__button");
@@ -8,8 +6,13 @@ const rightButton = document.querySelector(".right__button");
 let currentSlide = 0;
 let interval = null;
 let intervalTime = 5000; // in miliseconds
+let timeoutTime = 15000; // in miliseconds
 
-interval = setInterval(animate, intervalTime);
+
+
+function setTheInterval() {
+  interval = setInterval(animate, intervalTime);
+}
 
 function animate() {
   currentSlide++;
@@ -17,22 +20,35 @@ function animate() {
     currentSlide = 0;
   }
   heroSlideElements(currentSlide);
+  console.log('set')
 }
+setTheInterval();
 
 function nextSlide() {
   if(currentSlide < heroElements.length - 1){
     currentSlide++;
-  }  
+  } else {
+    currentSlide = 0;
+  }
+  console.log(currentSlide);
   clearInterval(interval);
   heroSlideElements(currentSlide);
+  clearTimeout(setTheInterval)
+  setTimeout(setTheInterval, timeoutTime);
 }
 
 function previousSlide() {
-  if(currentSlide > 0){
+  if(currentSlide === 0){    
+    currentSlide = heroElements.length - 1;
+    console.log(currentSlide);
+  } 
+  else {
     currentSlide--;
-  }  
+  }
   heroSlideElements(currentSlide);
   clearInterval(interval);
+  clearTimeout(setTheInterval);
+  setTimeout(setTheInterval, timeoutTime);
 }
 
 function heroSlideElements(index) {
