a94768def4a0a3c27e35e2e97efaed26e065704f
Maria <mariaromancz@gmail.com>
2019-10-28 14:32:49 +0100

    cookie finished!!!!!!!!!!

--- js/cookie.js
+++ js/cookie.js
@@ -1,6 +1,33 @@
-const buttonM = document.querySelector('#but');
+
+
 const cookieM = document.querySelector('.cookie');
-buttonM.addEventListener('click', function (){
-        cookieM.style = "display: none";
-    }
-    )
+
+
+
+function createCookie() {
+    cookieM.innerHTML = `
+        <a><h6>Ta strona wykorzystuje pliki cookie</h6>
+        <p>Używamy informacji zapisanych za pomocą plików cookies w celu zapewnienia 
+            maksymalnej wygody w korzystaniu z naszego serwisu. Mogą też korzystać z 
+            nich współpracujące z nami firmy badawcze oraz reklamowe. Jeżeli wyrażasz 
+            zgodę na zapisywanie informacji zawartej w cookies kliknij na &bdquo;x&rdquo; 
+            w prawym górnym rogu tej informacji. Jeśli nie wyrażasz zgody, ustawienia 
+            dotyczące plików cookies możesz zmienić w swojej przeglądarce.</p></a>
+        <button id="but">X</button>
+        `
+}
+
+if(localStorage.getItem('cookie') === null){
+    createCookie();
+    cookieM.style = "display: block";
+}
+
+
+const buttonM = document.querySelector('#but');
+
+
+buttonM.addEventListener('click', function () {
+    cookieM.style = "display: none";
+    localStorage.setItem('cookie', 'true');
+}
+)
