b7615c71fbf2532553d6c2c5abe4eac47c568ab6
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2019-11-08 16:36:30 +0100

    fixed email.js

--- js/email.js
+++ js/email.js
@@ -1,22 +1,25 @@
-const emailInput = document.querySelector("#email");
-const form = document.querySelector(".premiere__form");
-const submit = document.querySelector("#submit");
-const emailInfo = document.querySelector(".emailInfo");
+(function () {
+    const emailInput = document.querySelector("#email"),
+        form = document.querySelector(".premiere__form"),
+        submit = document.querySelector("#submit"),
+        emailInfo = document.querySelector(".emailInfo");
 
-function addToLocal() {
-    localStorage.setItem('email', true);
-}
+    function addToLocal() {
+        localStorage.setItem('email', true);
+    }
 
 
-if (localStorage.getItem('email') == "true") {
-    emailInfo.innerHTML = `
-        <p id="info">Dziękujemy za podanie maila <a href="./game.html">Teraz możesz zagrać w gre</a></p>
-        
-    `
-    setTimeout(() =>{
-        emailInfo.innerHTML = ``;
-        localStorage.setItem('email', false);
-    }, 5000);
-}
+    if (localStorage.getItem('email') == "true") {
+        emailInfo.innerHTML = `
+      <p id="info">Dziękujemy za podanie maila <a href="./game.html">Teraz możesz zagrać w gre</a></p>`
+      
+  
+        setTimeout(() => {
+            emailInfo.innerHTML = ``;
+            localStorage.setItem('email', false);
+        }, 5000);
+    }
 
-form.addEventListener("submit", addToLocal)
+    form.addEventListener("submit", addToLocal);
+})()
+
